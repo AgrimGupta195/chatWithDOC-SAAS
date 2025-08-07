4075d37f294c05b777ae3bf257285cf831e5c285
Agrim Gupta <agrimgupt195@gmail.com>
2025-08-07 20:47:54 +0530

    add lib files

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,20 +1,45 @@
-import { pgTable, serial, varchar, json, text, timestamp, pgEnum, integer } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const userSystemEnum = pgEnum('user_system_enum', ['user', 'system']);
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"]);
 
-export const chats = pgTable('chats', {
-    id: serial('id').primaryKey().notNull(),
-    pdfName: text('pdfName').notNull(),
-    pdfUrl: text('pdfUrl').notNull(),
-    createdAt: timestamp('createdAt').defaultNow().notNull(),
-    userId: varchar('userId', { length: 256 }).notNull(),
-    fileKey: text('fileKey').notNull(),
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(),
 });
 
-export const messages = pgTable('messages', {
-    id: serial('id').primaryKey().notNull(),
-    chatId: integer('chatId').references(() => chats.id).notNull(), // Changed to integer
-    content: text('content').notNull(),
-    role: userSystemEnum('role').notNull(),
-    createdAt: timestamp('createdAt').defaultNow().notNull(),
+export type DrizzleChat = typeof chats.$inferSelect;
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull(),
+});
+
+export const userSubscriptions = pgTable("user_subscriptions", {
+  id: serial("id").primaryKey(),
+  userId: varchar("user_id", { length: 256 }).notNull().unique(),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 256 })
+    .notNull()
+    .unique(),
+  stripeSubscriptionId: varchar("stripe_subscription_id", {
+    length: 256,
+  }).unique(),
+  stripePriceId: varchar("stripe_price_id", { length: 256 }),
+  stripeCurrentPeriodEnd: timestamp("stripe_current_period_ended_at"),
 });
